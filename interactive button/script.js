a481f457e295890102841a907fd73545462a9867
Sayvietha B.T <120291682+sayviethasayvie@users.noreply.github.com>
2024-06-21 10:16:12 +0530

    Update script.js

--- interactive button/script.js
+++ interactive button/script.js
@@ -1,1 +1,10 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.getElementById('colorButton');
 
+    button.addEventListener('click', () => {
+       
+        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+      
+        document.body.style.backgroundColor = randomColor;
+    });
+});
